d03e9f781fb58437c30d7609561fb0b41fc87b91
DESKTOP-OC07SRC\JoaquimGifre <jgifrero93@uoc.edu>
2020-10-31 13:20:22 +0100

    Adds canvas with player name text and starting script

---
+++ Multiplayer LAN/Assets/TankNaming.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using TMPro;
+using UnityEngine.UI;
+
+public class TankNaming : NetworkBehaviour
+{
+    public TextMeshProUGUI tmpro_tankName;
+
+    public const string startingName = "PLAYER";
+
+    [SyncVar(hook = "OnChangeTankName")]
+    public string currentName = startingName;
+
+    private GameObject input_playerNameGO;
+    private InputField inputField_playerName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            input_playerNameGO = GameObject.FindGameObjectWithTag("PlayerNameInputField");
+            inputField_playerName = input_playerNameGO.GetComponent<InputField>();
+            inputField_playerName.onValueChanged.AddListener(delegate { InputTextValueChanged(); });
+        }
+
+    }
+
+    private void OnChangeTankName(string oldName, string newName)
+    {
+        currentName = newName;
+
+        if (newName.Equals("")) currentName = startingName;
+
+        // Set the slider's value appropriately
+        tmpro_tankName.text = currentName;
+    }
+
+    private void InputTextValueChanged()
+    {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        // Update current tank name value
+        currentName = inputField_playerName.text;
+    }
+}
