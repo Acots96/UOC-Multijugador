4ce76c9e594cd4e649f4690b7ccd0f9773134a78
DESKTOP-OC07SRC\JoaquimGifre <jgifrero93@uoc.edu>
2020-11-30 20:22:07 +0100

    Error Messages and login input fields setting onChangeValue via script

---
+++ Multiplayer LAN/Assets/_Completed-Assets/Scripts/UI/MessageManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MessageManager : MonoBehaviour
+{
+    
+    public GameObject messageGO;
+    public TextMeshProUGUI tmp_message;
+    
+    private void ToggleMessageGameObject(bool value)
+    {
+        messageGO.SetActive(value);
+    }
+
+    public void ShowMessage(bool active, string message)
+    {
+        ToggleMessageGameObject(active);
+        tmp_message.text = message;
+    }
+
+    public void CloseMessage()
+    {
+        ToggleMessageGameObject(false);
+    }
+}
