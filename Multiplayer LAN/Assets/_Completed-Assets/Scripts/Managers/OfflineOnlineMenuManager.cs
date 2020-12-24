1e91530228c2622aceb482ee84ab7eed5498600b
Acots96 <acots96@gmail.com>
2020-12-24 14:09:42 +0100

    Offline mode without login, in a separate scene previous to the login.

---
+++ Multiplayer LAN/Assets/_Completed-Assets/Scripts/Managers/OfflineOnlineMenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OfflineOnlineMenuManager : MonoBehaviour {
+
+    public void StartLoginScene() {
+        SceneManager.LoadScene("Login");
+    }
+
+    public void StartOfflineScene() {
+        SceneManager.LoadScene("OfflineScene");
+    }
+
+}
